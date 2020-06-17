8b4cd8d87731ac0cf58779857e09aebfa1e1ff3b
Brandon LeBlanc <brandon@leblanc.codes>
2020-06-17 17:10:50 -0500

    use router

--- main.go
+++ main.go
@@ -1,44 +1,24 @@
 package main
 
 import (
-	"encoding/json"
 	"log"
 	"os"
-	"time"
 
 	"github.com/gobuffalo/packr/v2"
 
-	"github.com/demosdemon/weather/pkg/meteonook"
+	"github.com/demosdemon/weather/internal/router"
 )
 
 func main() {
 	box := packr.New("Data", "./data")
+	r := router.NewRouter(box)
 
-	bytes, err := box.Find("weather.wasm")
-	if err != nil {
-		log.Fatal(err)
+	port := "3000"
+	if v, ok := os.LookupEnv("PORT"); ok {
+		port = v
 	}
 
-	instance, err := meteonook.NewInstance(bytes)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	island := meteonook.Island{
-		Name:       "#island",
-		Hemisphere: meteonook.Northern,
-		Seed:       1160352294,
-		Timezone:   meteonook.Timezone{Location: time.Local},
-	}
-
-	day, err := island.NewDay(instance, time.Now())
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	enc := json.NewEncoder(os.Stdout)
-	enc.SetIndent("", "  ")
-	if err := enc.Encode(day); err != nil {
+	if err := r.Run(":" + port); err != nil {
 		log.Fatal(err)
 	}
 }
