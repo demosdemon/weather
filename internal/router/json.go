61eb44c139f7afe79b6703288f00b82aa420da5c
Brandon LeBlanc <brandon@leblanc.codes>
2020-06-17 20:21:31 -0500

    changeup JSON output

--- internal/router/json.go
+++ internal/router/json.go
@@ -1,17 +1,24 @@
 package router
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
 	"github.com/gobuffalo/packr/v2"
 	"github.com/memcachier/mc"
+
+	"github.com/demosdemon/weather/pkg/meteonook"
 )
 
 func getFeedJSON(c *mc.Client, box *packr.Box) gin.HandlerFunc {
 	return func(ctx *gin.Context) {
 		if days, _, err := getFeed(c, box, ctx); err == nil {
-			ctx.JSON(http.StatusOK, days)
+			var data = make(map[string]*meteonook.Day, len(days))
+			for _, day := range days {
+				data[fmt.Sprintf("%04d-%02d-%02d", day.Year, day.Month, day.Date)] = day
+			}
+			ctx.JSON(http.StatusOK, data)
 		}
 	}
 }
